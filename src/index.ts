9d92466013173550cacd6f7fade4de2127e32045
Chris Mitchell <chris@delacour.co.nz>
2025-06-02 18:12:52 -0700

    Remove android & web

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // Reexport the native module. On web, it will be resolved to LazerExpoAirplayModule.web.ts
 // and on native platforms to LazerExpoAirplayModule.ts
 export { default } from './LazerExpoAirplayModule';
-export { default as LazerExpoAirplayView } from './LazerExpoAirplayView';
 export * from  './LazerExpoAirplay.types';
